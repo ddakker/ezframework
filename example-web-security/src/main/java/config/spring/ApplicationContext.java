fc24c801a9ec88a5866c97b11ec8aac87a3a2259
ddakker <ddakker@gmail.com>
2016-04-21 15:48:33 +0900

    embedded db 적용

--- example-web-security/src/main/java/config/spring/ApplicationContext.java
+++ example-web-security/src/main/java/config/spring/ApplicationContext.java
@@ -19,21 +19,21 @@ import net.sf.log4jdbc.tools.LoggingType;
 @Configuration
 public class ApplicationContext {
 @Value("#{global['server.type']}")	private String serverType;
-	
-	@Bean
-	@Resource(name="jdbc/mariadb__tams")
+
+	/*@Bean
+	@Resource(name="jdbc/mariadb_tams")
 	public DataSource dataSource() {
 		DataSource dataSource = null;
-		
+
 		JndiDataSourceLookup dsLookup = new JndiDataSourceLookup();
 		dsLookup.setResourceRef(true);
 		try {
-			dataSource = dsLookup.getDataSource("java:comp/env/jdbc/mariadb__tams");
+			dataSource = dsLookup.getDataSource("java:comp/env/jdbc/mariadb_tams");
 			System.out.println("dataSource: " + dataSource);
 		} catch(Exception e){
 			System.err.println("Create Bean dataSource: " + e);
 		}
-		
+
 		if (dataSource == null) {
 			System.err.println("========== DATASOURCE NULL ==========");
 		} else {
@@ -44,17 +44,17 @@ public class ApplicationContext {
 			    	Log4JdbcCustomFormatter log4JdbcCustomFormatter = new Log4JdbcCustomFormatter();
 			    	log4JdbcCustomFormatter.setLoggingType(LoggingType.MULTI_LINE);
 			    	log4JdbcCustomFormatter.setSqlPrefix("SQL     :\n\t\t");
-			    	
+
 			    	Log4jdbcProxyDataSource log4jdbcProxyDataSource = new Log4jdbcProxyDataSource(dataSource);
 			    	log4jdbcProxyDataSource.setLogFormatter(log4JdbcCustomFormatter);
-			    	
+
 			    	dataSource = log4jdbcProxyDataSource;
 			    } catch(Exception e){
 			    	System.err.println(e);
 			    }
 			}
 		}
-	    
+
 	    return dataSource;
-	}
+	}*/
 }
